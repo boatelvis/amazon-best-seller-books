78e86a902f2dcec8d32e53df6d37472b8a4cd367
Elvis Boateng <eboat086@gmail.com>
2023-04-10 07:56:57 +0000

    setup booklist component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
+    author: 'Dav Pilkey',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+
+  {
+    title: 'Lessons in chemistry:',
+    author: 'Bonnie Garmus',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+]
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />
+      })}
+    </section>
+  )
+}
+
+const Book = ({ img, title, author }) => {
+  const displayTitle = () => {
+    console.log(title)
+  }
+
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <button onClick={displayTitle}>Display title</button>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList />)
